9cb41a92be3a6b25104218c7771c5ecff1ed8a09
saruni <timothysaruni13@gmail.com>
2024-07-07 20:00:33 +0300

    adds crud lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 
 from models import db, Plant
@@ -10,7 +11,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
 migrate = Migrate(app, db)
 db.init_app(app)
 
